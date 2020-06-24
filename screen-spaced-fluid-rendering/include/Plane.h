9a556c5088c67e658c59eefd066d785aac1530bf
Marcus Hultman <marcus.hultman1@gmail.com>
2020-06-24 12:12:33 +0200

    refactor Plane

--- screen-spaced-fluid-rendering/include/Plane.h
+++ screen-spaced-fluid-rendering/include/Plane.h
@@ -1,32 +1,18 @@
 #pragma once
 
-#include <stdlib.h>
-#include <stdio.h>
-
-#include <string>
-
 #include <GL/glew.h>
-
 #include <glm/matrix.hpp>
-#include <glm/gtc/type_ptr.hpp>
 
-#include <IL/il.h>
+class Plane {
+ public:
+  Plane(float size);
+  Plane(float width, float height);
+  ~Plane();
 
-#include "textfile.h"
+  void draw(const glm::mat4 &view, const glm::mat4 &proj);
 
-
-class Plane
-{
-private:
-	GLuint m_VAO;
-	GLuint m_texture;
-	GLuint m_shader;
-public:
-	Plane(float size);
-	Plane(float width, float height);
-	~Plane();
-	void draw(const glm::mat4 view, const glm::mat4 proj);
-private:
-	void load(float width, float height);
+ private:
+  GLuint _vertex_array_object;
+  GLuint _texture;
+  GLuint _program;
 };
-
--- screen-spaced-fluid-rendering/src/Plane.cpp
+++ screen-spaced-fluid-rendering/src/Plane.cpp
@@ -1,138 +1,114 @@
 #include "Plane.h"
 
-Plane::Plane(float size)
-	: Plane(size, size)
-{
-}
+#include <IL/il.h>
+#include <glm/gtc/type_ptr.hpp>
+#include "textfile.h"
 
-Plane::Plane(float width, float height)
-{
-	load(width, height);
-}
+Plane::Plane(float size) : Plane(size, size) {}
 
+Plane::Plane(float width, float height) {
+  const float positions[] = {
+      width, 0, height, -width, 0, height, width, 0, -height, -width, 0, -height};
+  const float tex_coords[] = {0, 0, 1, 0, 0, 1, 1, 1};
+  const int indices[] = {0, 1, 2, 2, 1, 3};
 
-Plane::~Plane()
-{
-}
+  glGenVertexArrays(1, &_vertex_array_object);
+  glBindVertexArray(_vertex_array_object);
 
-void Plane::load(float width, float height)
-{
-	const float positions [] = {
-		// X	Y	Z
-		width, 0, height,
-		-width, 0, height,
-		width, 0, -height,
-		-width, 0, -height,
-	};
+  GLuint buffer;
 
-	const float texCoords [] = {
-		0, 0,
-		1, 0,
-		0, 1,
-		1, 1,
-	};
+  glGenBuffers(1, &buffer);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	const int indices [] = {
-		0, 1, 2,
-    2, 1, 3,
-	};
+  glGenBuffers(1, &buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, buffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-	glGenVertexArrays(1, &m_VAO);
-	glBindVertexArray(m_VAO);
-
-	GLuint buffer;
-
-	glGenBuffers(1, &buffer);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	glGenBuffers(1, &buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-
-	glGenBuffers(1, &buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(texCoords), texCoords, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
+  glGenBuffers(1, &buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, buffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(tex_coords), tex_coords, GL_STATIC_DRAW);
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
   glBindVertexArray(0);
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
-	// Load shader
-	
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  // Load shader
 
-	auto vv = textFileRead("screen-spaced-fluid-rendering/resource/shaders/simple.vert");
-	auto ff = textFileRead("screen-spaced-fluid-rendering/resource/shaders/simple.frag");
+  auto vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+  auto fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+
+  auto vv = textFileRead("screen-spaced-fluid-rendering/resource/shaders/simple.vert");
+  auto ff = textFileRead("screen-spaced-fluid-rendering/resource/shaders/simple.frag");
 
   auto p = vv.c_str();
-	glShaderSource(vertexShader, 1, &p, NULL);
+  glShaderSource(vertex_shader, 1, &p, NULL);
   p = ff.c_str();
-	glShaderSource(fragmentShader, 1, &p, NULL);
+  glShaderSource(fragment_shader, 1, &p, NULL);
 
-	glCompileShader(vertexShader);
-	glCompileShader(fragmentShader);
+  glCompileShader(vertex_shader);
+  glCompileShader(fragment_shader);
 
-	int compileOK;
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &compileOK);
-  assert(compileOK);
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &compileOK);
-  assert(compileOK);
+  int ok;
+  glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &ok);
+  assert(ok);
+  glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &ok);
+  assert(ok);
 
-	m_shader = glCreateProgram();
-	glAttachShader(m_shader, vertexShader);
-	glAttachShader(m_shader, fragmentShader);
-	glLinkProgram(m_shader);
-	glValidateProgram(m_shader);
+  _program = glCreateProgram();
+  glAttachShader(_program, vertex_shader);
+  glAttachShader(_program, fragment_shader);
+  glLinkProgram(_program);
+  glValidateProgram(_program);
 
-	// Load texture
+  // Load texture
 
-	glGenTextures(1, &m_texture);
-	glBindTexture(GL_TEXTURE_2D, m_texture);
+  glGenTextures(1, &_texture);
+  glBindTexture(GL_TEXTURE_2D, _texture);
 
-	ILuint image = ilGenImage();
-	ilBindImage(image);
+  ILuint image = ilGenImage();
+  ilBindImage(image);
 
-	ilLoadImage("screen-spaced-fluid-rendering/resource/ground.jpg");
-	ilConvertImage(IL_RGB, IL_UNSIGNED_BYTE);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,
-      ilGetInteger(IL_IMAGE_WIDTH),
-      ilGetInteger(IL_IMAGE_HEIGHT),
-      0,
-      GL_RGB,
-      GL_UNSIGNED_BYTE,
-      ilGetData());
-	ilDeleteImage(image);
+  ilLoadImage("screen-spaced-fluid-rendering/resource/ground.jpg");
+  ilConvertImage(IL_RGB, IL_UNSIGNED_BYTE);
+  glTexImage2D(GL_TEXTURE_2D,
+               0,
+               GL_RGB,
+               ilGetInteger(IL_IMAGE_WIDTH),
+               ilGetInteger(IL_IMAGE_HEIGHT),
+               0,
+               GL_RGB,
+               GL_UNSIGNED_BYTE,
+               ilGetData());
+  ilDeleteImage(image);
 
-	// Set the filtering mode
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  // Set the filtering mode
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 }
 
-void Plane::draw(const glm::mat4 view, const glm::mat4 proj)
-{
-	glUseProgram(m_shader);
+Plane::~Plane() = default;
 
-	glDisable(GL_CULL_FACE);
-	glEnable(GL_DEPTH_TEST);
+void Plane::draw(const glm::mat4 &view, const glm::mat4 &proj) {
+  glUseProgram(_program);
 
-	// Send the VP to the vertex shader
-	glUniformMatrix4fv(glGetUniformLocation(m_shader, "view"), 
-		1, GL_FALSE, glm::value_ptr(view));
-	glUniformMatrix4fv(glGetUniformLocation(m_shader, "projection"),
-		1, GL_FALSE, glm::value_ptr(proj));
+  glDisable(GL_CULL_FACE);
+  glEnable(GL_DEPTH_TEST);
 
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, m_texture);
-	glUniform1i(glGetUniformLocation(m_shader, "uTexture"), 0);
+  glUniformMatrix4fv(glGetUniformLocation(_program, "view"), 1, GL_FALSE, glm::value_ptr(view));
+  glUniformMatrix4fv(
+      glGetUniformLocation(_program, "projection"), 1, GL_FALSE, glm::value_ptr(proj));
 
-	glBindVertexArray(m_VAO);
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, _texture);
+  glUniform1i(glGetUniformLocation(_program, "uTexture"), 0);
 
-	glUseProgram(0);
+  glBindVertexArray(_vertex_array_object);
+  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+  glUseProgram(0);
 }
