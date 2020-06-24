be7de2e9700bd9a58096cada5612778f993378e4
Marcus Hultman <marcus.hultman1@gmail.com>
2020-06-24 12:20:27 +0200

    refactor SkyBox

--- screen-spaced-fluid-rendering/include/SkyBox.h
+++ screen-spaced-fluid-rendering/include/SkyBox.h
@@ -1,29 +1,20 @@
 #pragma once
 
-#include <string>
-
 #include <GL/glew.h>
-
 #include <glm/glm.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
-#include <IL/il.h>
-
-#include "textfile.h"
-
 #include "Box.h"
 
-class SkyBox
-{
-private:
-	Box m_box;
-	GLuint m_texture;
-	GLuint m_shader;
-public:
-	SkyBox();
-	~SkyBox();
-	void draw(const glm::mat4 view, const glm::mat4 proj);
-private:
-	void load();
-};
+class SkyBox {
+ public:
+  SkyBox();
+  ~SkyBox();
 
+  void draw(const glm::mat4 &view, const glm::mat4 &proj);
+
+ private:
+  void load();
+
+  Box _box;
+  GLuint _texture;
+  GLuint _program;
+};
--- screen-spaced-fluid-rendering/src/SkyBox.cpp
+++ screen-spaced-fluid-rendering/src/SkyBox.cpp
@@ -1,109 +1,96 @@
 #include "SkyBox.h"
 
-SkyBox::SkyBox() : m_box(1)
-{
-	load();
-}
+#include <string>
+#include <vector>
+#include <IL/il.h>
+#include <glm/gtc/type_ptr.hpp>
+#include "config.h"
+#include "textfile.h"
 
+SkyBox::SkyBox() : _box(1) {
+  auto vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+  auto fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
 
-SkyBox::~SkyBox()
-{
-}
-
-
-void SkyBox::load()
-{
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER); 
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-
-	auto vv = textFileRead("screen-spaced-fluid-rendering/resource/shaders/skybox.vert");
-	auto ff = textFileRead("screen-spaced-fluid-rendering/resource/shaders/skybox.frag");
+  auto vv = textFileRead(config::kResourcesDir + "/shaders/skybox.vert");
+  auto ff = textFileRead(config::kResourcesDir + "/shaders/skybox.frag");
 
   auto p = vv.c_str();
-	glShaderSource(vertexShader, 1, &p, NULL);
+  glShaderSource(vertex_shader, 1, &p, NULL);
   p = ff.c_str();
-	glShaderSource(fragmentShader, 1, &p, NULL);
+  glShaderSource(fragment_shader, 1, &p, NULL);
 
-	glCompileShader(vertexShader);
-	glCompileShader(fragmentShader);
-	
-	m_shader = glCreateProgram();
-	glAttachShader(m_shader, vertexShader);
-	glAttachShader(m_shader, fragmentShader);
-	glLinkProgram(m_shader);
-	glValidateProgram(m_shader);
+  glCompileShader(vertex_shader);
+  glCompileShader(fragment_shader);
 
-	// Load texture cube
+  _program = glCreateProgram();
+  glAttachShader(_program, vertex_shader);
+  glAttachShader(_program, fragment_shader);
+  glLinkProgram(_program);
+  glValidateProgram(_program);
 
-	glGenTextures(1, &m_texture);
-	glBindTexture(GL_TEXTURE_CUBE_MAP, m_texture);
+  // Load texture cube
 
-	GLenum face [] = {
-		GL_TEXTURE_CUBE_MAP_NEGATIVE_X, 
-		GL_TEXTURE_CUBE_MAP_POSITIVE_X, 
+  glGenTextures(1, &_texture);
+  glBindTexture(GL_TEXTURE_CUBE_MAP, _texture);
 
-		GL_TEXTURE_CUBE_MAP_POSITIVE_Y,
-		GL_TEXTURE_CUBE_MAP_NEGATIVE_Y,
-		
-		GL_TEXTURE_CUBE_MAP_NEGATIVE_Z,
-		GL_TEXTURE_CUBE_MAP_POSITIVE_Z,
-	};
-	std::string imageFilename [] = {
-		"screen-spaced-fluid-rendering/resource/envmap_miramar/miramar_bk.tga",
-		"screen-spaced-fluid-rendering/resource/envmap_miramar/miramar_ft.tga",
+  std::vector<std::pair<GLenum, std::string>> faces = {
+      {GL_TEXTURE_CUBE_MAP_NEGATIVE_X, "miramar_bk.tga"},
+      {GL_TEXTURE_CUBE_MAP_POSITIVE_X, "miramar_ft.tga"},
 
-		"screen-spaced-fluid-rendering/resource/envmap_miramar/miramar_dn.tga",
-		"screen-spaced-fluid-rendering/resource/envmap_miramar/miramar_up.tga",
-		
-		"screen-spaced-fluid-rendering/resource/envmap_miramar/miramar_lf.tga",
-		"screen-spaced-fluid-rendering/resource/envmap_miramar/miramar_rt.tga",
-	};
+      {GL_TEXTURE_CUBE_MAP_POSITIVE_Y, "miramar_dn.tga"},
+      {GL_TEXTURE_CUBE_MAP_NEGATIVE_Y, "miramar_up.tga"},
 
-	ILuint imageIds[6];
-	ilGenImages(6, imageIds);
-	for (unsigned int i = 0; i < 6; i++){
-		ilBindImage(imageIds[i]);
+      {GL_TEXTURE_CUBE_MAP_NEGATIVE_Z, "miramar_lf.tga"},
+      {GL_TEXTURE_CUBE_MAP_POSITIVE_Z, "miramar_rt.tga"},
+  };
 
-		ilLoadImage(imageFilename[i].c_str());
-		ilConvertImage(IL_RGBA, IL_UNSIGNED_BYTE);
+  ILuint image_ids[faces.size()];
+  ilGenImages(faces.size(), image_ids);
 
-		glTexImage2D(face[i], 0, GL_RGBA,
-			ilGetInteger(IL_IMAGE_WIDTH),
-			ilGetInteger(IL_IMAGE_HEIGHT),
-			0, GL_RGBA, GL_UNSIGNED_BYTE,
-			ilGetData());
-	}
-	ilDeleteImages(6, imageIds);
+  for (auto i = 0; i < faces.size(); ++i) {
+    auto &[face, filename] = faces[i];
+    ilBindImage(image_ids[i]);
 
-	// Set the filtering mode
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    ilLoadImage((config::kResourcesDir + "/envmap_miramar/" + filename).c_str());
+    ilConvertImage(IL_RGBA, IL_UNSIGNED_BYTE);
+    glTexImage2D(face,
+                 0,
+                 GL_RGBA,
+                 ilGetInteger(IL_IMAGE_WIDTH),
+                 ilGetInteger(IL_IMAGE_HEIGHT),
+                 0,
+                 GL_RGBA,
+                 GL_UNSIGNED_BYTE,
+                 ilGetData());
+  }
+  ilDeleteImages(faces.size(), image_ids);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 }
 
-void SkyBox::draw(const glm::mat4 view, const glm::mat4 proj)
-{
-	glUseProgram(m_shader);
+SkyBox::~SkyBox() = default;
 
-	glDisable(GL_CULL_FACE);
-	glDisable(GL_DEPTH_TEST);
+void SkyBox::draw(const glm::mat4 &view, const glm::mat4 &proj) {
+  glUseProgram(_program);
 
-	glUniformMatrix4fv(glGetUniformLocation(m_shader, "view"),
-		1, GL_FALSE, glm::value_ptr(view));
+  glDisable(GL_CULL_FACE);
+  glDisable(GL_DEPTH_TEST);
 
-	glUniformMatrix4fv(glGetUniformLocation(m_shader, "projection"),
-		1, GL_FALSE, glm::value_ptr(proj));
+  glUniformMatrix4fv(glGetUniformLocation(_program, "view"), 1, GL_FALSE, glm::value_ptr(view));
 
-	// Extract camera position
-	glm::vec3 cameraPosition = glm::vec3(glm::inverse(view)[3]);
+  glUniformMatrix4fv(
+      glGetUniformLocation(_program, "projection"), 1, GL_FALSE, glm::value_ptr(proj));
 
-	glUniform3fv(glGetUniformLocation(m_shader, "cameraPosition"),
-		1, glm::value_ptr(cameraPosition));
+  // Extract camera position
+  auto camera_pos = glm::vec3(glm::inverse(view)[3]);
 
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_CUBE_MAP, m_texture);
-	glUniform1i(glGetUniformLocation(m_shader, "uTexture"), 0);
-	
-	m_box.draw();
+  glUniform3fv(glGetUniformLocation(_program, "cameraPosition"), 1, glm::value_ptr(camera_pos));
 
-	glUseProgram(0);
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_CUBE_MAP, _texture);
+  glUniform1i(glGetUniformLocation(_program, "uTexture"), 0);
+
+  _box.draw();
+
+  glUseProgram(0);
 }
